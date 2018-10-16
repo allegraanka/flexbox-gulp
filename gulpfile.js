61bcb09a1572c668c1d8e5b02bc3b7fcd8700fb3
Allegra Anka <allegra.anka@gmail.com>
2018-10-15 22:25:18 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function(){
+  gulp.src('css/styles.css')
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('build'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('css/styles.css', ['styles']);
+});
